2c003e7009827315483b2ac95ec084d7adf60a72
wadi-sudo <62206997+wadi-sudo@users.noreply.github.com>
2022-02-27 13:36:36 +0100

    Update Control.OSMGeocoder.js

--- leaflet-control-osm-geocoder/Control.OSMGeocoder.js
+++ leaflet-control-osm-geocoder/Control.OSMGeocoder.js
@@ -6,7 +6,7 @@ if (typeof console == "undefined") {
 L.Control.OSMGeocoder = L.Control.extend({
 	options: {
 		collapsed: true,
-		position: 'topright',
+		position: 'topleft',
 		text: 'Locate',
 		placeholder: '',
 		bounds: null, // L.LatLngBounds
